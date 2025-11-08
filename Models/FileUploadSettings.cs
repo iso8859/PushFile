87db187cce8fd71f270564d0828fbc1e3d3028ba
Rémi THOMAS <remi@xtware.com>
2025-11-08 05:55:40 +0100

    Tested version

--- Models/FileUploadSettings.cs
+++ Models/FileUploadSettings.cs
@@ -7,5 +7,4 @@ public class FileUploadSettings
 {
     public string DestinationDirectory { get; set; } = string.Empty;
     public int MaxFileSizeMB { get; set; } = 100;
-    public List<string> AllowedExtensions { get; set; } = new();
 }
